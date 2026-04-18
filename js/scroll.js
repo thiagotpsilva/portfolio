4dcd4565af58aa90176d19a2bd019ef8d33b4187
Thiago Pereira <ti.silper93@gmail.com>
2026-04-18 19:02:03 -0300

    Clean some files and insert code in about.html

--- js/scroll.js
+++ js/scroll.js
@@ -1,20 +1,20 @@
-$('#backTop').on('click', function (e) {
-  if (this.hash !== '') {
+$("#backTop").on("click", function (e) {
+  if (this.hash !== "") {
     e.preventDefault();
 
     const hash = this.hash;
 
-    $('html, body').animate(
+    $("html, body").animate(
       {
         scrollTop: $(hash).offset().top - 100,
       },
       // 1200
-      800
+      800,
     );
   }
 });
 
-window.addEventListener('scroll', function () {
-  var scroll = this.document.querySelector('.scrollTop');
-  scroll.classList.toggle('active', window.scrollY > 10);
+window.addEventListener("scroll", function () {
+  var scroll = this.document.querySelector(".scrollTop");
+  scroll.classList.toggle("active", window.scrollY > 10);
 });
